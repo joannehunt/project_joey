b321e21a43732144f937fc3ef103b831d59940f3
Joey Hunt <JHUNT37@emory.edu>
2025-04-30 14:51:14 -0400

    updated code folder

--- code/100_render_report.R
+++ code/100_render_report.R
@@ -1,9 +1,6 @@
 here::i_am("code/100_render_report.R")
 
-library(rmarkdown)
-report_filename<- paste0(
-  "Uploadable_Project_Draft.html"
-)
+
+
 #rendering in production mode
-render("Final_Project_Draft.Rmd",
-       output_file = report_filename)
+rmarkdown::render(here::here("Uploadable_Project_Draft.Rmd"))
